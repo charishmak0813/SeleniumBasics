945c3a2ae13ca2e1daba803de117554a6fd6d345
Charishma Kolisetty <charishmak0813@gmail.com>
2026-01-04 12:55:05 -0500

    Changes to driver

--- src/test/java/Basics/ScreenShot.java
+++ src/test/java/Basics/ScreenShot.java
@@ -25,8 +25,6 @@ public class ScreenShot {
                 + "/ScreenShots/section.png");
         FileUtils.copyFile(sourceFile, targetLocation);
         driver.quit();
-        FileUtils.copyFile(sourceFile, targetFile1);
-        //driver.quit();
-        driver.close();
+        //driver.close();
     }
 }
