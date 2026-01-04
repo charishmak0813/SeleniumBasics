7ed463dd27039771906134acd781102c07d23979
Charishma Kolisetty <charishmak0813@gmail.com>
2026-01-04 12:22:15 -0500

    Changes to target location field

--- src/test/java/Basics/ScreenShot.java
+++ src/test/java/Basics/ScreenShot.java
@@ -21,9 +21,9 @@ public class ScreenShot {
         File sourceFile = section.getScreenshotAs(OutputType.FILE);
 
         // Save to target location
-        File targetFile1 = new File(System.getProperty("user.dir")
+        File targetLocation = new File(System.getProperty("user.dir")
                 + "/ScreenShots/section.png");
-        FileUtils.copyFile(sourceFile, targetFile1);
+        FileUtils.copyFile(sourceFile, targetLocation);
         driver.quit();
     }
 }
